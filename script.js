09b7db3826e51cba4594d60a20b46d557aa4c4a8
Sebastian <sebas.c1309@gmail.com>
2021-08-03 19:21:27 -0600

    second day

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+let canvasSize = 16;
+let i = 0;
+let pixels = undefined;
+let allPixels = document.getElementsByClassName("pixel");
+
 const body = document.body;
 body.style.height = "100vh";
 body.style.margin = "0";
@@ -16,35 +21,65 @@ buttonsSection.style.display = "flex";
 buttonsSection.style.justifyContent = "center";
 buttonsSection.style.alignItems = "center";
 
+let rangeSlider = document.createElement("input");
+buttonsSection.append(rangeSlider);
+rangeSlider.className = "slider";
+rangeSlider.id = "number-of-pixels";
+rangeSlider.type = "range";
+rangeSlider.min = "1";
+rangeSlider.max = "32";
+rangeSlider.value = "16";
+numberOfPixels = document.getElementById("number-of-pixels");
+
 const restartButton = document.createElement("button");
 restartButton.addEventListener("click", cleanCanvas);
 buttonsSection.append(restartButton);
 restartButton.textContent = "Reset";
+restartButton.className = "clean-button-hover";
 restartButton.id = "clean-button";
 
-const container = document.createElement("div");
+let container = document.createElement("div");
 body.append(container);
-container.style.display = "grid";
-container.style.gridTemplateColumns = "repeat(16, 1fr)";
-container.style.gridTemplateRows = "repeat(16, 1fr)";
-container.style.border = "solid";
-container.style.margin = "0";
-container.style.height = "500px";
-container.style.width = "500px";
 
 
-let i = 1;
-let pixels = undefined;
-while (i <= 256) {
-    pixels = document.createElement("div");
-    container.append(pixels);
-    pixels.style.border = "solid";
-    pixels.style.borderWidth = "0.5px";
-    pixels.className = "pixel";
-    pixels.id = "pixel-" + i;
-    pixels.addEventListener("mouseover", paintPixel);
-    i ++;
+function createCanvas() {
+    i = 1;
+    // This part sets the grid size
+    container.style.display = "grid";
+    container.style.gridTemplateColumns = "repeat("+canvasSize+", 1fr)";
+    container.style.gridTemplateRows = "repeat("+canvasSize+", 1fr)";
+    container.style.border = "solid";
+    container.style.margin = "0";
+    container.style.height = "500px";
+    container.style.width = "500px";
+    while (i <= canvasSize*canvasSize) {
+        // This part sets the pixel's properties
+        pixels = document.createElement("div");
+        container.append(pixels);
+        pixels.style.background = "white";
+        pixels.style.border = "solid";
+        pixels.style.borderWidth = "0.5px";
+        pixels.className = "pixel";
+        pixels.id = "pixel-" + i;
+        pixels.addEventListener("mouseover", paintPixel);
+        i ++;
+    }
 }
+
+function cleanCanvas() {
+    i = 0;
+    while (i <= canvasSize*canvasSize) {
+        allPixels[i].style.background = "white";
+        i ++; 
+    }
+}
+
+numberOfPixels.oninput = function getNumberOfPixels() {
+    canvasSize = this.value
+    createCanvas();
+    console.log(canvasSize);
+  }
+
 
 function paintPixel(e) {
     singlePixel = document.getElementById(e.target.id);
@@ -52,11 +87,5 @@ function paintPixel(e) {
     singlePixel.style.background = "red";
 }
 
-function cleanCanvas() {
-    i = 0;
-    allPixels = document.getElementsByClassName("pixel");
-    while (i <= 256) {
-        allPixels[i].style.background = "white";
-        i ++; 
-    }
-}
+
+createCanvas();
