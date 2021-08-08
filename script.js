9a37a35ff994ff271813f8eb0964341ce8eaa3d7
Sebastian <sebas.c1309@gmail.com>
2021-08-07 18:46:42 -0600

    made it look preatty but not functional yet

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-let canvasSize = 16;
-let i = 0;
-let pixels = undefined;
-let allPixels = document.getElementsByClassName("pixel");
-
 const body = document.body;
 body.style.height = "100vh";
 body.style.margin = "0";
@@ -12,80 +7,98 @@ body.style.flexDirection = "column";
 body.style.justifyContent = "center";
 body.style.alignItems = "center";
 
-const buttonsSection = document.createElement("div");
-body.append(buttonsSection);
-buttonsSection.className = "buttons";
-buttonsSection.style.height = "100px";
-buttonsSection.style.width = "50%";
-buttonsSection.style.display = "flex";
-buttonsSection.style.justifyContent = "center";
-buttonsSection.style.alignItems = "center";
+let frame = document.createElement("div");
+body.append(frame);
+frame.style.background = "crimson";
+frame.style.boxShadow = "5px 5px black"
+frame.style.border = "solid";
+frame.style.borderRadius = "2%"
+frame.style.margin = "0px";
+frame.style.height = "600px";
+frame.style.width = "700px";
+frame.style.display = "flex";
+frame.style.flexDirection = "column";
+frame.style.justifyContent = "space-evenly";
+frame.style.alignItems = "center";
 
-let rangeSlider = document.createElement("input");
-buttonsSection.append(rangeSlider);
-rangeSlider.className = "slider";
-rangeSlider.id = "number-of-pixels";
-rangeSlider.type = "range";
-rangeSlider.min = "1";
-rangeSlider.max = "32";
-rangeSlider.value = "16";
-numberOfPixels = document.getElementById("number-of-pixels");
+let titleSection = document.createElement("h1");
+frame.append(titleSection);
+titleSection.id = "title-section";
+titleSection.textContent = "Etch a Sketch";
+titleSection.style.color = "#FFD02C";
+titleSection.style.textShadow = "2px 1px black";
+titleSection.style.fontSize = "60px";
+titleSection.style.marginTop = "0";
+titleSection.style.marginBottom = "0";
+titleSection.style.display = "flex";
+titleSection.style.alignItems = "center";
+
+let container = document.createElement("div");
+frame.append(container);
+container.className = "draw-container";
+container.style.background = "blue";
+container.style.border = "solid";
+container.style.borderRadius = "5%";
+container.style.margin = "0px";
+container.style.height = "400px";
+container.style.width = "500px";
+container.style.display = "grid";
+
+let pixelSquare = document.createElement("div");
+container.append(pixelSquare);
+pixelSquare.style.backgroundColor = "lightgray";
+pixelSquare.style
+
+let circlesSection = document.createElement("div");
+frame.append(circlesSection);
+circlesSection.className = "circles-section";
+circlesSection.style.height = "100px";
+circlesSection.style.width = "100%";
+circlesSection.style.position = "relative";
+circlesSection.style.display = "flex";
+circlesSection.style.alignItems = "center";
+circlesSection.style.justifyContent = "space-around";
+
+let leftCircle = document.createElement("div");
+circlesSection.append(leftCircle);
+leftCircle.className = "circles";
+
+let slider = document.createElement("input");
+circlesSection.append(slider);
+slider.className = "slider";
+slider.id = "number-of-pixels";
+slider.type = "range";
+slider.min = "2";
+slider.max = "32";
+slider.value = "16";
+slider.style.boxShadow = "2px 2px black"
+slider.oninput = getUiSliderValue();
 
 const restartButton = document.createElement("button");
-restartButton.addEventListener("click", cleanCanvas);
-buttonsSection.append(restartButton);
+circlesSection.append(restartButton);
 restartButton.textContent = "Reset";
 restartButton.className = "clean-button-hover";
 restartButton.id = "clean-button";
+restartButton.style.boxShadow = "2px 2px black"
 
-let container = document.createElement("div");
-body.append(container);
+let rightCircle = document.createElement("div");
+circlesSection.append(rightCircle);
+rightCircle.className = "circles";
 
+let circles = document.getElementsByClassName("circles");
 
-function createCanvas() {
-    i = 1;
-    // This part sets the grid size
-    container.style.display = "grid";
-    container.style.gridTemplateColumns = "repeat("+canvasSize+", 1fr)";
-    container.style.gridTemplateRows = "repeat("+canvasSize+", 1fr)";
-    container.style.border = "solid";
-    container.style.margin = "0";
-    container.style.height = "500px";
-    container.style.width = "500px";
-    while (i <= canvasSize*canvasSize) {
-        // This part sets the pixel's properties
-        pixels = document.createElement("div");
-        container.append(pixels);
-        pixels.style.background = "white";
-        pixels.style.border = "solid";
-        pixels.style.borderWidth = "0.5px";
-        pixels.className = "pixel";
-        pixels.id = "pixel-" + i;
-        pixels.addEventListener("mouseover", paintPixel);
-        i ++;
-    }
+let i = 0;
+while (i < 2) {
+    circles[i].style.background = "whitesmoke";
+    circles[i].style.height = "70px";
+    circles[i].style.width = "70px";
+    circles[i].style.borderRadius = "50%";
+    circles[i].style.boxShadow = "2px 2px black"
+    i ++;
 }
 
-function cleanCanvas() {
-    i = 0;
-    while (i <= canvasSize*canvasSize) {
-        allPixels[i].style.background = "white";
-        i ++; 
-    }
+container.style.gridTemplateColumns = "repeat("+16+", 1fr)";
+container.style.gridTemplateRows = "repeat("+16+", 1fr)";
+function getUiSliderValue() {
+    console.log("hu");
 }
-
-numberOfPixels.oninput = function getNumberOfPixels() {
-    canvasSize = this.value
-    createCanvas();
-    console.log(canvasSize);
-  }
-
-
-function paintPixel(e) {
-    singlePixel = document.getElementById(e.target.id);
-    console.log(singlePixel);
-    singlePixel.style.background = "red";
-}
-
-
-createCanvas();
